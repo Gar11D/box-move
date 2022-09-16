c4f3a70621cd3d7bbb8973dfc4c2c8f27a52f63d
Harry Dobbs <hrdobbs@outlook.com>
2022-09-16 17:33:52 +0100

    adds first array

--- boxMove.js
+++ boxMove.js
@@ -1,4 +1,17 @@
 //boxMove.js
+// document.getElementById("array-box1").innerHTML = fruitArray
+
+// const fruitList= ['apples', 'bananas', 'oranges', 'pears'];
+
+// $(document).ready(function () {
+//     const fruitListArray = [];
+//     for (i=0; i<fruitList.length; i++) {
+//         fruitListArray+=  fruitList[i]  + '<br />';
+//     }
+//     document.getElementById("array-box1").innerHTML = fruitListArray;
+//     // songList="";
+// }); 
+
 const testFunction = () => {
     document.getElementById("item-box1").style.display === "none" ? (
         document.getElementById("item-box1").style.display = "block",
@@ -8,3 +21,7 @@ const testFunction = () => {
         document.getElementById("item-box2").style.display = "block"
     )
 }
+
+
+
+
