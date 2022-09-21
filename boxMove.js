99ce11eecce3f5164910762548b498f78335da57
Harry Dobbs <hrdobbs@outlook.com>
2022-09-21 05:48:08 +0100

    tidies the code presentation and removes unnecessary comments

--- boxMove.js
+++ boxMove.js
@@ -1,17 +1,3 @@
-//boxMove.js
-// document.getElementById("array-box1").innerHTML = fruitArray
-
-// const fruitList= ['apples', 'bananas', 'oranges', 'pears'];
-
-// $(document).ready(function () {
-//     const fruitListArray = [];
-//     for (i=0; i<fruitList.length; i++) {
-//         fruitListArray+=  fruitList[i]  + '<br />';
-//     }
-//     document.getElementById("array-box1").innerHTML = fruitListArray;
-//     // songList="";
-// }); 
-
 const testFunction = () => {
     document.getElementById("item-box1").style.display === "none" ? (
         document.getElementById("item-box1").style.display = "block",
